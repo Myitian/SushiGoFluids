fd09eb52f77e9b7553bb2ee723f4c2eaaf644073
Myitian <miaoyitian233@gmail.com>
2025-01-20 21:38:12 +0800

    fix recipe ingredient

--- src/main/java/net/myitian/sushigofluids/SushiGoFluids.java
+++ src/main/java/net/myitian/sushigofluids/SushiGoFluids.java
@@ -16,30 +16,28 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
 
 @Mod(SushiGoFluids.MOD_ID)
 public class SushiGoFluids {
     public static final String MOD_ID = "sushigofluids";
-    public static final Logger LOGGER = LogManager.getLogger();
-    public static final HashMap<AmountItem, VirtualFluid> FLUID_HASH_MAP = new HashMap<>();
-    public static final ResourceLocation AMOUNT_CAPABILITY = new ResourceLocation(SushiGoFluids.MOD_ID, "amount");
+    public static final HashMap<AmountItem, VirtualFluid> FLUID_ITEM_MAP = new HashMap<>();
+    public static final ResourceLocation AMOUNT_CAPABILITY = new ResourceLocation(MOD_ID, "amount");
+    public static final ResourceLocation MAIN_TEXTURE = new ResourceLocation(MOD_ID, "block/fluid");
 
     public static final DeferredRegister<Fluid> FLUIDS = DeferredRegister.create(ForgeRegistries.FLUIDS, MOD_ID);
     public static final RegistryObject<VirtualFluid> SOY_SAUCE = FLUIDS.register("soy_sauce",
             () -> new VirtualFluid(FluidType.Properties.create()
                     .descriptionId("item.sushigocrafting.soy_sauce")
                     .temperature(20),
-                    new ResourceLocation(MOD_ID, "block/fluid"),
+                    MAIN_TEXTURE,
                     0xEE2D1D17));
     public static final RegistryObject<VirtualFluid> WASABI_PASTE = FLUIDS.register("wasabi_paste",
             () -> new VirtualFluid(FluidType.Properties.create()
                     .descriptionId("item.sushigocrafting.wasabi_paste")
                     .temperature(20),
-                    new ResourceLocation(MOD_ID, "block/fluid"),
+                    MAIN_TEXTURE,
                     0xFF92A348));
 
     public SushiGoFluids() {
@@ -50,8 +48,8 @@ public class SushiGoFluids {
     }
 
     public void onFMLCommonSetup(FMLCommonSetupEvent event) {
-        FLUID_HASH_MAP.put(SushiContent.Items.SOY_SAUCE.get(), SOY_SAUCE.get());
-        FLUID_HASH_MAP.put(SushiContent.Items.WASABI_PASTE.get(), WASABI_PASTE.get());
+        FLUID_ITEM_MAP.put(SushiContent.Items.SOY_SAUCE.get(), SOY_SAUCE.get());
+        FLUID_ITEM_MAP.put(SushiContent.Items.WASABI_PASTE.get(), WASABI_PASTE.get());
     }
 
     @SubscribeEvent
@@ -59,7 +57,7 @@ public class SushiGoFluids {
         ItemStack itemStack = event.getObject();
         if (!(itemStack.getItem() instanceof AmountItem amountItem))
             return;
-        VirtualFluid fluid = FLUID_HASH_MAP.get(amountItem);
+        VirtualFluid fluid = FLUID_ITEM_MAP.get(amountItem);
         if (fluid == null)
             return;
         event.addCapability(AMOUNT_CAPABILITY, new AmountCapabilityProvider(itemStack, amountItem, fluid));
